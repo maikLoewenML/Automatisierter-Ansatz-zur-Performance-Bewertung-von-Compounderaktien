9f8d1e7d2f75cfe77b5e4494feb7fb515c6202a4
Maik Löwen <66090909+maikLoewenML@users.noreply.github.com>
2023-08-29 16:16:48 +0200

    Jahre für jede Renditekategorie

--- S&P 500.py
+++ S&P 500.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yfinance as yf
 import requests
+import matplotlib.pyplot as plt
 
 url = "https://pkgstore.datahub.io/core/s-and-p-500-companies/constituents_json/data/297344d8dc0a9d86b8d107449c851cc8/constituents_json.json"  # Ersetzen Sie dies durch die tatsächliche URL des JSON
 
@@ -14,6 +15,9 @@ else:
     print(f"Fehler beim Abrufen des JSON: {response.status_code}")
 
 successful_stocks = []
+
+# Ein Dictionary zur Speicherung der Renditen und den entsprechenden Jahren
+returns_years_mapping = {k: [] for k in range(-40, 70, 10)}
 
 for stock in stocks:
     name = stock["Name"]
@@ -30,6 +34,39 @@ for stock in stocks:
 
     annual_return = history['Close'].resample('Y').ffill().pct_change()
 
+    # Hier fügen wir die Jahreszahlen den entsprechenden Renditebereichen zu
+    for year, value in annual_return.dropna().items():
+        if value < -0.4:
+            returns_years_mapping[-40].append(year.year)
+        elif value < -0.3:
+            returns_years_mapping[-30].append(year.year)
+        elif value < -0.2:
+            returns_years_mapping[-20].append(year.year)
+        elif value < -0.1:
+            returns_years_mapping[-10].append(year.year)
+        elif value < 0.0:
+            returns_years_mapping[0].append(year.year)
+        elif value < 0.1:
+            returns_years_mapping[10].append(year.year)
+        elif value < 0.2:
+            returns_years_mapping[20].append(year.year)
+        elif value < 0.3:
+            returns_years_mapping[30].append(year.year)
+        elif value < 0.4:
+            returns_years_mapping[40].append(year.year)
+        elif value < 0.5:
+            returns_years_mapping[50].append(year.year)
+        elif value < 0.6:
+            returns_years_mapping[60].append(year.year)
+        elif value >= 0.6:
+            returns_years_mapping[60].append(year.year)
+        else:
+            key = int(value // 0.1 * 10) * 10
+            if key in returns_years_mapping:
+                returns_years_mapping[key].append(year.year)
+            else:
+                print(f"Unerwartete Rendite gefunden: {value * 100}% für {year.year}")
+
     total_return = 0
     years_counted = 0
 
@@ -42,6 +79,26 @@ for stock in stocks:
         average_return = total_return / years_counted
         if average_return >= 0.15:
             successful_stocks.append(stock_symbol)
+
+# Balkendiagramm erstellen
+x = list(returns_years_mapping.keys())
+y = [] # [len(years) for years in returns_years_mapping.values()]
+
+bars = plt.bar(x, y, align='center', width=8)
+
+# Jahreszahlen auf den Balken anzeigen
+for bar, years in zip(bars, returns_years_mapping.values()):
+    height = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/2, height, ', '.join(map(str, years)), ha='center', va='bottom', rotation=90)
+
+plt.xticks(x, [f"{val}%" for val in x])
+plt.xlabel('Jährliche Rendite')
+plt.title('Jahre für jede Renditekategorie')
+plt.grid(axis='y')
+
+# Anzeigen
+plt.tight_layout()
+plt.show()
 
 if successful_stocks:
     print("Folgende Aktien hatten eine durchschnittliche jährliche Rendite von 15% oder höher:")
@@ -60,14 +117,14 @@ for stock in successful_stocks:
         end_price = history['Close'].iloc[-1]   # Endpreis
 
         # Rendite für diese Aktie berechnen
-        stock_return = end_price / start_price
+        cagr = ((end_price / start_price) ** (1 / 14)) - 1
 
         # Rendite zur Gesamtsumme hinzufügen
-        sum_returns += stock_return
+        sum_returns += cagr
+
     except Exception as e:
         print(f"Konnte keine aktuellen Daten für {stock} abrufen: {e}")
 
 # Durchschnittsrendite in Prozent berechnen
-average_return_percentage = (sum_returns / len(successful_stocks) - 1) * 100
-
-print(f"Die durchschnittliche Rendite für alle erfolgreichen Aktien von 2009 bis heute beträgt {average_return_percentage:.2f}%.")
+average_cagr = sum_returns / len(successful_stocks) * 100
+print(f"Die durchschnittliche jährliche Rendite für alle erfolgreichen Aktien von 2009 bis heute beträgt {average_cagr:.2f}%.")
