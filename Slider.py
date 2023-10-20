8e1da7e7a43cc30a590d12e6f8f8e813ac2a4629
maikLoewenML <loewenmaik@gmail.com>
2023-10-20 08:28:55 +0200

    Slider-Implementierung funktioniert

--- Slider.py
+++ Slider.py
@@ -1,12 +1,17 @@
 import json
-import plotly.graph_objects as go
+import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# JSON-Datei einlesen
-with open('results.json', 'r') as f:
-    data = json.load(f)
+# Load the data
+with open("results.json", "r") as file:
+    data = json.load(file)
 
+start_years = sorted(set(entry['start_jahr'] for entry in data))
+anlagehorizonts = sorted(set(entry['anlagehorizont'] for entry in data))
+aktie_lengths = sorted(set(entry['aktie_laenge_am_markt'] for entry in data))
+renditen = sorted(set(entry['durchschnittliche_rendite'] for entry in data))
 
-# Daten filtern basierend auf den Werten der Slider
 def filter_data(start_jahr, anlagehorizont, aktie_laenge, rendite):
     return [
         entry for entry in data
@@ -16,65 +21,51 @@ def filter_data(start_jahr, anlagehorizont, aktie_laenge, rendite):
         and entry['durchschnittliche_rendite'] == rendite
     ]
 
-# Erstellen Sie ein interaktives Diagramm
-fig = go.Figure()
+def update_plot():
+    start_jahr = start_year_slider.get()
+    anlagehorizont = anlagehorizont_slider.get()
+    aktie_laenge = aktie_length_slider.get()
+    rendite = rendite_slider.get()
 
-filtered_data = filter_data(data[0]['start_jahr'], data[0]['anlagehorizont'], data[0]['aktie_laenge_am_markt'], data[0]['durchschnittliche_rendite'])
-years = sorted(filtered_data[0]['average_yearly_returns'].keys())
-avg_rendites = [filtered_data[0]['average_yearly_returns'][year] for year in years]
-fig.add_trace(go.Scatter(x=years, y=avg_rendites, mode='lines'))
+    filtered_data = filter_data(start_jahr, anlagehorizont, aktie_laenge, rendite)
 
-# Sliders
-start_years = sorted(set(entry['start_jahr'] for entry in data))
-anlagehorizonts = sorted(set(entry['anlagehorizont'] for entry in data))
-aktie_lengths = sorted(set(entry['aktie_laenge_am_markt'] for entry in data))
-renditen = sorted(set(entry['durchschnittliche_rendite'] for entry in data))
+    ax.clear()
 
-fig.update_layout(
-    updatemenus=[
-        go.layout.Updatemenu(
-            type="buttons",
-            showactive=True,
-            y=1,
-            buttons=list([
-                dict(label=str(year),
-                     method="restyle",
-                     args=[{"x": [sorted(filter_data(year, data[0]['anlagehorizont'], data[0]['aktie_laenge_am_markt'], data[0]['durchschnittliche_rendite'])[0]['average_yearly_returns'].keys())],
-                            "y": [list(filter_data(year, data[0]['anlagehorizont'], data[0]['aktie_laenge_am_markt'], data[0]['durchschnittliche_rendite'])[0]['average_yearly_returns'].values())]}]) for year in start_years
-            ])
-        ),
-        go.layout.Updatemenu(
-            type="buttons",
-            showactive=True,
-            y=0.9,
-            buttons=list([
-                dict(label=str(horizont),
-                     method="restyle",
-                     args=[{"x": [sorted(filter_data(data[0]['start_jahr'], horizont, data[0]['aktie_laenge_am_markt'], data[0]['durchschnittliche_rendite'])[0]['average_yearly_returns'].keys())],
-                            "y": [list(filter_data(data[0]['start_jahr'], horizont, data[0]['aktie_laenge_am_markt'], data[0]['durchschnittliche_rendite'])[0]['average_yearly_returns'].values())]}]) for horizont in anlagehorizonts
-            ])
-        ),
-        go.layout.Updatemenu(
-            type="buttons",
-            showactive=True,
-            y=0.8,
-            buttons=list([
-                dict(label=str(laenge),
-                     method="restyle",
-                     args=[{"x": [sorted(filter_data(data[0]['start_jahr'], data[0]['anlagehorizont'], laenge, data[0]['durchschnittliche_rendite'])[0]['average_yearly_returns'].keys())],
-                            "y": [list(filter_data(data[0]['start_jahr'], data[0]['anlagehorizont'], laenge, data[0]['durchschnittliche_rendite'])[0]['average_yearly_returns'].values())]}]) for laenge in aktie_lengths
-            ])
-        ),
-        go.layout.Updatemenu(
-            type="buttons",
-            showactive=True,
-            y=0.7,
-            buttons=list([
-                dict(label=str(r),
-                     method="restyle",
-                     args=[{"x": [sorted(filter_data(data[0]['start_jahr'], data[0]['anlagehorizont'], data[0]['aktie_laenge_am_markt'], r)[0]['average_yearly_returns'].keys())],
-                            "y": [list(filter_data(data[0]['start_jahr'], data[0]['anlagehorizont'], data[0]['aktie_laenge_am_markt'], r)[0]['average_yearly_returns'].values())]}]) for r in renditen
-            ])
-        ),
-    ]
-)
+    if not filtered_data:
+        ax.plot([], [])
+    else:
+        years = sorted(filtered_data[0]['average_yearly_returns'].keys())
+        avg_rendites = [filtered_data[0]['average_yearly_returns'][year] for year in years]
+        ax.plot(years, avg_rendites, marker='o')
+
+    # Update the figure
+    ax.set_title('Durchschnittliche jährliche Renditen')
+    ax.set_xlabel('Jahr')
+    ax.set_ylabel('Durchschnittliche Rendite (%)')
+    ax.grid(True)
+    canvas.draw()
+
+root = tk.Tk()
+root.title("Data Analysis")
+
+start_year_slider = tk.Scale(root, from_=min(start_years), to=max(start_years), resolution=1, orient=tk.HORIZONTAL, label="Startjahr")
+start_year_slider.pack()
+
+anlagehorizont_slider = tk.Scale(root, from_=min(anlagehorizonts), to=max(anlagehorizonts), resolution=1, orient=tk.HORIZONTAL, label="Anlagehorizont")
+anlagehorizont_slider.pack()
+
+aktie_length_slider = tk.Scale(root, from_=min(aktie_lengths), to=max(aktie_lengths), resolution=1, orient=tk.HORIZONTAL, label="Aktienlänge")
+aktie_length_slider.pack()
+
+rendite_slider = tk.Scale(root, from_=min(renditen), to=max(renditen), resolution=0.1, orient=tk.HORIZONTAL, label="Rendite")
+rendite_slider.pack()
+
+fig = Figure(figsize=(8, 6), dpi=100)
+ax = fig.add_subplot(111)
+canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+update_button = tk.Button(root, text="Update Plot", command=update_plot)
+update_button.pack()
+
+root.mainloop()
